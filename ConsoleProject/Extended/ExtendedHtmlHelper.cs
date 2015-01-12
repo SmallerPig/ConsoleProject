6b10dd0dc0fa17f1a61f072458d5b94fc8fd6f0f
SmallerPig <zhuqiyancheng@126.com>
2015-01-12 21:07:47 +0800

    增加对后台编辑input的拓展方法

---
+++ ConsoleProject/Extended/ExtendedHtmlHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace ConsoleProject.Extended
+{
+    public static class ExtendedHtmlHelper
+    {
+
+        public static MvcHtmlString EditModelProp(this HtmlHelper htmlHelper,string showname, string prop, string value ="" ,string tip = "",string type="text")
+        {
+            string result = "<div class='contentUnit'>" +
+                                "<div class='title'>" + showname + "</div>" +
+                                "<div class='content'>" +
+                                    "<input type='" + type + "' class='inputText' name= '" + prop + "' value = '"+ value +"'/>" +
+                                    "</div>" + (string.IsNullOrEmpty(tip)?"":"<div class='tip'>"+tip+"</div>") +
+                            "</div>";
+
+            return MvcHtmlString.Create(result);
+        }
+    }
+}
