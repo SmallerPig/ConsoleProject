f1d4c66f0351558876e5d39ee334a16e2a415a4e
smallerpig <zhuqiyancheng@hotmail.com>
2015-06-16 14:00:50 +0800

    使用log4.net记录程序异常

--- ConsoleProject/App_Start/FilterConfig.cs
+++ ConsoleProject/App_Start/FilterConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web;
 using System.Web.Mvc;
+using ConsoleProject.Filter;
 
 namespace ConsoleProject
 {
@@ -7,7 +8,7 @@ namespace ConsoleProject
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new LogExceptionFileAttribute());
         }
     }
 }
