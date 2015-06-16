b9dd149be46a6488fa2d29d3d4a8ff149a681238
smallerpig <zhuqiyancheng@hotmail.com>
2015-06-16 11:06:27 +0800

    [修改]删除越来的actionName属性

--- ConsoleProject/Areas/Admin/Models/SmpPager.cs
+++ ConsoleProject/Areas/Admin/Models/SmpPager.cs
@@ -8,21 +8,26 @@
         /// <summary>
         /// 构造函数
         /// </summary>
-        /// <param name="actionName">action的名称</param>
         /// <param name="count">总计的条目数量</param>
         /// <param name="pageIndex">当前页码</param>
-        public SmpPager(string actionName, int count, int pageIndex)
+        public SmpPager(int count, int pageIndex)
         {
-            this.ActionName = actionName;
+            //this.ActionName = actionName;
             this.Count = count;
             this.PageIndex = pageIndex;
         }
 
         public int PageIndex { get; set; }
 
-        public string ActionName { get; set; }
+        //public string ActionName { get; set; }
 
         public int Count { get; set; }
+
+
+        public int GetPageCount(int pageSize = 20)
+        {
+            return (Count % pageSize == 0) ? Count / pageSize : Count / pageSize + 1;
+        }
 
     }
 }
