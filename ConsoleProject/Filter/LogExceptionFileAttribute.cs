fad55bf654dceeb85367d159909a9d4cefaa3af3
smallerpig <zhuqiyancheng@hotmail.com>
2015-06-16 14:04:17 +0800

    添加log4.net处理Attribute文件

---
+++ ConsoleProject/Filter/LogExceptionFileAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConsoleProject.Filter
+{
+    public class LogExceptionFileAttribute : HandleErrorAttribute
+    {
+        readonly log4net.ILog log = log4net.LogManager.GetLogger("WebLogger");
+        public override void OnException(ExceptionContext filterContext){
+
+            //处理错误消息，将其跳转到一个页面
+            log.Error(filterContext.RequestContext.HttpContext.Request.Url);
+            log.Error(filterContext.Exception.ToString());
+            //log.Fatal("fatal信息", filterContext.Exception);
+            base.OnException(filterContext);
+        }
+
+    }
+}
